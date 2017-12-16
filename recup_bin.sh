15084108afcc792f1db8297b9762e4c44f482415
Anthony BARA <banthony@e2r8p14.42.fr>
2017-12-16 19:56:55 +0100

    Reorganisation du repo

--- recup_bin.sh
+++ recup_bin.sh
@@ -6,9 +6,13 @@
 #    By: banthony <banthony@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/11/28 14:58:09 by banthony          #+#    #+#              #
-#    Updated: 2017/11/28 18:30:31 by banthony         ###   ########.fr        #
+#    Updated: 2017/12/16 19:56:05 by banthony         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-file /usr/bin/* | grep Mach | cut -d : -f 1,1 | cut -d '(' -f 1,1 >> usr_bin.txt
+#
+#	Recupere le path de tout les fichier de /usr/bin contenant "Mach"
+#	Permet de recuperer le path uniquement des fichier contenant des mach-o (fat y compris)
+
+file /usr/bin/* | grep "Mach" | cut -d : -f 1,1 | cut -d '(' -f 1,1 >> usr_bin.txt
 cat ./usr_bin.txt | xargs -n 1 otool -hv
